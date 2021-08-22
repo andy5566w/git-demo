7e7ca536a6af69b2d22e8fbf4ccb39dd8e4db4f0
andy.tsai <andy.tsai@deephow.com>
2021-08-22 21:15:38 +0800

    upload typeOF

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-console.log('11111')
-console.log('22222')
-console.log('33333')
+// console.log('11111')
+// console.log('22222')
+// console.log('33333')
+function typeOF(target){
+    return Object.prototype.toString.call(target).slice(8, -1).toLowerCase()
+}
+
+console.log(typeOF([]))
+console.log(typeOF({}))
+console.log(typeOF(new Date()))
+console.log(typeOF(null))
