9b965c94e58f44f66f22796532710a0947f3b541
andy.tsai <andy.tsai@deephow.com>
2021-08-22 21:34:32 +0800

    prototype

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ function typeOF(target){
     return Object.prototype.toString.call(target).slice(8, -1).toLowerCase()
 }
 
-console.log(typeOF([]))
-console.log(typeOF({}))
-console.log(typeOF(new Date()))
-console.log(typeOF(null))
+// console.log(typeOF([]))
+// console.log(typeOF({}))
+// console.log(typeOF(new Date()))
+// console.log(typeOF(null))
+
+function Animal(){
+    this.color = ['green', 'grey', 'white']
+}
+
+function Dog(){
+    this.__proto__.constructor(this)
+}
+Dog.prototype = new Animal()
+Dog.prototype.constructor = Animal
+
+const d = new Dog()
+console.log(d.color);
